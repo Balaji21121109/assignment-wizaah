0377c9ec84c58abb6dd2a947baf4f5f48680bc67
Balaji21121109 <93375333+Balaji21121109@users.noreply.github.com>
2022-11-26 09:52:54 +0530

    last

--- script.js
+++ script.js
@@ -5,28 +5,13 @@ function increment() {
     document.getElementById('demoInput').stepUp();
 }
 function decrement() {
-    document.getElementById('demoInput').stepDown();
+    document.getElementById('demoInput').stepDown(1);
 }
 
-// ===================================Dropdown================
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
 
 // =====================================addtocart ==============
 
 function addtocart() {
-    document.getElementById("img3").src = "images/icon-badge.png"; 
+   
+  alert(" The product has been added to the cart ")
 }
